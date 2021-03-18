3357b1377d42e1553ea04ef6863dea8c5dac46c0
Hermann Hanf <Hermann.Hanf@digital-one.org>
2021-03-18 17:35:04 +0100

    added a segmentation for bmi

--- Insurance.py
+++ Insurance.py
@@ -23,19 +23,42 @@ with open("insurance.csv", newline="") as data:
     for row in data:
         individuals.append(row)
 
-        age.append(row["age"])
+        age.append(int(row["age"]))
         sex.append(row["sex"])
-        bmi.append(row["bmi"])
-        children.append(row["children"])
+        bmi.append(float(row["bmi"]))
+        children.append(int(row["children"]))
         smoker.append(row["smoker"])
         region.append(row["region"])
-        charges.append(row["charges"])
+        charges.append(float(row["charges"]))
 
         counter += 1
 
-class bmi:
-    pass
+# classifier for bmi
+class bmi_classification:
+    # aggregates the bmi values into different segments
+    bmi_segments = {"thin": [], "normal": [], "fat": [], "very fat": []}
 
-print(individuals)
-print(age)
-print(counter)
+    # shows the distribution of the different within the total dataset
+    bmi_segments_distribution = {"thin": 0, "normal": 0, "fat": 0, "very fat": 0}
+    
+    for i in bmi:
+        if i < 18.5:
+            bmi_segments["thin"].append(i)
+        elif (i >= 18.5) and (i < 24.9):
+            bmi_segments["normal"].append(i)
+        elif (i >= 24.9) and (i < 29.9):
+            bmi_segments["fat"].append(i)
+        else:
+            bmi_segments["very fat"].append(i)
+    
+    bmi_segments_distribution["thin"] = round(len(bmi_segments["thin"])/counter*100, 2)
+    bmi_segments_distribution["normal"] = round(len(bmi_segments["normal"])/counter*100, 2)
+    bmi_segments_distribution["fat"] = round(len(bmi_segments["fat"])/counter*100, 2)
+    bmi_segments_distribution["very fat"] = round(len(bmi_segments["very fat"])/counter*100, 2)
+
+    print(bmi_segments_distribution["thin"])
+    print(bmi_segments_distribution["normal"])
+    print(bmi_segments_distribution["fat"])
+    print(bmi_segments_distribution["very fat"])
+
+bmi_classification = bmi_classification()
